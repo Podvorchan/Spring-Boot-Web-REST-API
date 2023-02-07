5b2244fca10f50c71563198b46e4c7e965900e59
Podvorchan <porubo@ukr.net>
2023-02-07 21:51:28 +0200

    v.2

--- src/main/resources/db/migration/V001__init_migration.sql
+++ src/main/resources/db/migration/V001__init_migration.sql
@@ -16,5 +16,4 @@ CREATE TABLE IF NOT EXISTS my_store.order
     id               integer NOT NULL DEFAULT next-val('my_store.my_store_id_seq'),
     creation_date    date,
     cost             double,
-    fk_products_id   integer constraint data_source_fk_product_id_fkey references my_store.product,
-     PRIMARY KEY(id)
+      PRIMARY KEY(id)
